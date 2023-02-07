00b87ae733f60e838d91ecaee8ee03eac1ed9779
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-07 20:50:37 +0530

    ours manager students is working fine

--- app/models.py
+++ app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(max_length=30)
     roll=models.IntegerField()
+    #as we know that object is default manager 
+    #if we want to change it
+    #suppose we want to make student as the manager
+    students=models.Manager()
--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Student
 # Create your views here.
 def fun(request):
-    student_data=Student.objects.all()
+    student_data=Student.students.all()
+    # student_data=Student.objects.all()
+
     #here object is bydefault manager and this is important point
     return render(request,'app/home.html',{'student_data':student_data})
