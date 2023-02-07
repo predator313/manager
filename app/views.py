c5cc9d16a28fb3b2a416a59db96f47ec3f5ee292
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-07 20:45:04 +0530

    objects is default manager

--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from .models import Student
 # Create your views here.
 def fun(request):
     student_data=Student.objects.all()
+    #here object is bydefault manager and this is important point
     return render(request,'app/home.html',{'student_data':student_data})
