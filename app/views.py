ed196439b0173956c5a011c3039999c67ef357fb
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-07 20:42:41 +0530

    retriving data from admin panel to homepage

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def fun(request):
-    return render(request,'app/home.html')
+    student_data=Student.objects.all()
+    return render(request,'app/home.html',{'student_data':student_data})
